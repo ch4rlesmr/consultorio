bbe0ff80f8d5cc81ce420156bb3e9592a621027d
carlos medina <karloz@xaviiz.xaviiz.net>
2016-09-12 17:49:31 -0500

    publicacion de archivos en el repo GitHub

---
+++ consultorio/gulpfile.js
@@ -0,0 +1,25 @@
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+/*elixir(mix => {
+    mix.sass('app.scss')
+       .webpack('app.js');
+});*/
+
+elixir(function (mix) {
+	// mix.sass(['app.scss'], 'public/css/libs/libs.css');
+	mix.sass('app.scss');
+	mix.scripts(['libs/jquery.min.js', 'libs/bootstrap.min.js', 'libs/nprogress.js', 'libs/fastclick.js'], 'public/js/libs/libs.js');
+});
---
+++ consultorio/resources/views/layouts/general-layout/app_base.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <!-- Head del documento, se incluye estilos y propios y del template -->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Consultorio | Login</title>
+
+    <!-- <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <link href="../build/css/custom.min.css" rel="stylesheet"> -->
+    <link href="css/libs/libs.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+
+    @yield('content')
+
+    <!-- <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <script src="../vendors/Chart.js/dist/Chart.min.js"></script>
+    <script src="../build/js/custom.min.js"></script> -->
+
+    <script href="js/libs/libs.js"></script>
+  </body>
+
+</html>
